e07d59f34fe8a4ad8045b3f1609fcb0382d16d00
araimov06 <raimovadhamjon@gmail.com>
2026-09-24 21:47:23 +0500

    Update week_2.dart
    Removed multiple main functions and consolidated code.

--- week_2.dart
+++ week_2.dart
@@ -1,1 +1,31 @@
+// task 1
+void main(List <String > arguments ) {
+    print('Hello , Dart World!');
+    if (arguments.isNotEmpty) {
+        print('Command line arguments passed : ${arguments .join(", ")}');
+    }
+}
+
+void main(){
+    print("Adxamjon Raimov");
+    print("240422");
+    print("Software engineering");
+}
+
+void main(List<String> arguments) {
+  print('${arguments.length}');
+}
+
+void main(List<String> arguments) {
+    List<int> numbers = arguments.map(int.parse).toList();
+    double t = 0;
+    for (int i in numbers){
+        t+=i;
+    }
+    t=t/numbers.length;
+    print(t);
+}
+
+
+//task 2
 
